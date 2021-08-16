94512b8c5856d96683986d1016786f33110782e1
Erwin <erkro1@hotmail.com>
2021-08-16 13:54:35 +0200

    Fix restart after migration

--- src/Migration.java
+++ src/Migration.java
@@ -155,7 +155,54 @@ public class Migration extends CordovaPlugin {
 
     private void restartCordova(){
         Log.d(TAG, "restarting Cordova activity");
-        activity.recreate();
+        doColdRestart();
+    }
+
+    /**
+     * Performs a full cold app restart - restarts application
+     * https://stackoverflow.com/a/22345538/777265
+     */
+    protected void doColdRestart() {
+        String baseError = "Unable to cold restart application: ";
+        try {
+            logInfo("Cold restarting application");
+            Context c = applicationContext;
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        //mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //create a pending intent so the application is restarted after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity(c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        Log.i(TAG,"Killing application for cold restart");
+                        //kill the application
+                        System.exit(0);
+                    } else {
+                        handleError(baseError+"StartActivity is null");
+                    }
+                } else {
+                    handleError(baseError+"PackageManager is null");
+                }
+            } else {
+                handleError(baseError+"Context is null");
+            }
+        } catch (Exception ex) {
+            handleError(baseError+ ex.getMessage());
+        }
     }
 
 
